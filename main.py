c89e409208bab0bcebf717d864dae21b2f6ae089
Aaron <sergeantbeast456@gmail.com>
2020-03-25 11:49:48 -0400

    Completed the basic functionality of loading an image as a arguement

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 from PIL import Image
+import sys
 
-im = Image.open("stock_photo.jpg")
-print(im.format, im.size, im.mode)
+def main():
+    if len(sys.argv) == 2:
+        im = Image.open(sys.argv[1])
+        print("Successfully loaded image!")
+        width, height = im.size
+        print("Image size: " + str(height) + " x " + str(width))
+    elif len(sys.argv) > 2:
+        print("Please only enter one image name")
+    else:
+        print("Please pass the name of the image as an arguement.")
+
+if __name__ == "__main__":
+    main()
