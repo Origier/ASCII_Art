f827c2d61910cf106264cd45cbfd22fcaa77dc24
Aaron <sergeantbeast456@gmail.com>
2020-03-25 12:36:36 -0400

    fixed a minor bug with the width and height being in reverse

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def getRGBs(image):
     returns the two dimensional array of RGB's for each x and y coordinate of the image
     """
     values = list(image.getdata())
-    width, height = image.size
+    height, width = image.size
     valuesArray = []
     counter = 0
     for y in range(height):
@@ -26,15 +26,10 @@ def main():
         try:
             im = Image.open(sys.argv[1])
             print("Successfully loaded image!")
-            width, height = im.size
+            height, width = im.size
             print("Image size: " + str(height) + " x " + str(width))
             imageArray = getRGBs(im)
             print("Successfully constructed pixel matrix!")
-            print("Iterating through pixel contents: ")
-            for y in range(height):
-                for x in range(width):
-                    print(imageArray[y][x], end = "")
-                print('\n')
         except Exception as ex:
             print(ex)
     elif len(sys.argv) > 2:
